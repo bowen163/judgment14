1b4e6e25b8fbcdb86ff758e1cdbc9aaebccbd1fb
bowen163 <763893371@qq.com>
2019-07-06 10:25:30 +0800

    Initial Commit

---
+++ judgment14/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  judgment14
+//
+//  Created by s20181102936 on 2019/7/6.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
